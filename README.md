fb0420268de199c90987bae17687443f5c75e035
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-06-29 17:14:01 +0300

    add __doc__

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# parquet_convertor
-🧱 Convert csv to parquet and explore parquet data structure
+# Data Files Convertor
+
+This script allows the user to convert different data types. 
+
+This tool accepts comma separated value files (.csv) as well as apache parquet (.parquet) files. It is assumed that the first row of the spreadsheet is the location of the columns.
+
+This script requires that `pandas`, `argparse`, `pyarrow`, `csv` and `json` be installed within the Python environment you are running this script in.
+
+This file can also be imported as a module and contains the following functions:
+
+* csv_to_parquet - convert csv to parquet and save to file
+* parquet_to_csv - convert parquet to csv and save to file
+* csv_to_json - convert csv to json and save to file
+* parquet_schema - returns schema of parquet file
+* add_filename_suffix - returns filename string with added suffix for filename and change extension
+* is_file_ext_correct - returns returns True if filename has correct file extension and prints message otherwise
+* main - the main function of the script
--- convertor.py
+++ convertor.py
@@ -1,3 +1,26 @@
+"""Data Files Convertor
+
+This script allows the user to convert different data types. 
+
+This tool accepts comma separated value files (.csv) as well as apache parquet
+(.parquet) files. It is assumed that the first row of the spreadsheet is the
+location of the columns.
+
+This script requires that `pandas`, `argparse`, `pyarrow`, `csv` and `json` be installed within the Python
+environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * csv_to_parquet - convert csv to parquet and save to file
+    * parquet_to_csv - convert parquet to csv and save to file
+    * csv_to_json - convert csv to json and save to file
+    * parquet_schema - returns schema of parquet file
+    * add_filename_suffix - returns filename string with added suffix for filename and change extension
+    * is_file_ext_correct - returns returns True if filename has correct file extension and prints message otherwise
+    * main - the main function of the script
+"""
+
 # import the necessary packages
 import pandas as pd
 import csv
@@ -35,7 +58,7 @@ def csv_to_json(csv_path: str, json_path: str, json_indent=4):
 def parquet_schema(parquet_path: str):
     """Get schema of parquet file"""
     df = pd.read_parquet(parquet_path)
-    print(pyarrow.Table.from_pandas(df=df).schema)
+    return pyarrow.Table.from_pandas(df=df).schema
 
 
 # define functions for working with filenames
@@ -57,7 +80,7 @@ def is_file_ext_correct(parameter: str, filename: str, extension: str) -> bool:
 def main():
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser(
-        description='Script for converting CSV, PARQUET and JSON files.')
+        description=__doc__)
     ap.add_argument("-cp", "--csv2parquet", type=str,
                     help="Convert csv to parquet. Set input csv filename string (example: data.csv)")
     ap.add_argument("-pc", "--parquet2csv", type=str,
@@ -112,7 +135,7 @@ def main():
     elif args['get_schema']:
         # get schema of parquet
         if is_file_ext_correct('get_schema', args['get_schema'], 'parquet'):
-            parquet_schema(args['get_schema'])
+            print(parquet_schema(args['get_schema']))
     else:
         print('Please, pass the necessary arguments for convertion (example: --csv2parquet data.csv).')
         print('Type --help for description of parameters.')
