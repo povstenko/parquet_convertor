f5725bb54ed994ae84b4c50d758cc2902bb2996a
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-06-29 15:36:14 +0300

    functions description

--- convertor.py
+++ convertor.py
@@ -3,26 +3,19 @@ import pandas as pd
 import csv, json
 import argparse
 from pathlib import Path
- 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-cp", "--csv2parquet", required=False, help="Convert csv to parquet")
-ap.add_argument("-pc", "--parquet2csv", required=False, help="Convert parquet to csv")
-ap.add_argument("-cj", "--csv2json", required=False, help="Convert csv to json")
-ap.add_argument("-s", "--get-schema", required=False, help="Get schema of parquet file")
-args = vars(ap.parse_args())
-
-# print(args)
 
 def csv_to_parquet(csv_path: str, parquet_path: str):
+    """Сonvert csv to parquet and save to file"""
     df = pd.read_csv(csv_path)
     df.to_parquet(parquet_path)
 
 def parquet_to_csv(parquet_path: str, csv_path: str):
+    """Сonvert parquet to csv and save to file"""
     df = pd.read_parquet(parquet_path)
     df.to_csv(csv_path)
 
 def csv_to_json(csv_path:str, json_path:str, json_indent=4):
+    """Сonvert csv to json and save to file"""
     data = []
     
     with open(csv_path) as csvFile:
@@ -31,35 +24,48 @@ def csv_to_json(csv_path:str, json_path:str, json_indent=4):
 
     with open(json_path, 'w') as jsonFile:
         jsonFile.write(json.dumps(data, indent=json_indent))
-        
+
+def add_filename_suffix(filename:str, suffix:str, extension:str)->str:
+    """Add suffix for filename and change extension"""
+    return filename.split('.')[0] + '_' + suffix + '.' + extension
+
 def is_file_ext_correct(parameter:str, filename:str, extension:str)-> bool:
+    """Returns True if filename has correct file extension and prints message otherwise"""
     if filename.split('.')[1] != extension:
-        print('Wrong argument for --csv2parquet. You must specify *.csv file for input')
+        print(f'Wrong argument for --{parameter}. You must specify *.{extension} file for input')
         return False
     else:
         return True
 
 def main():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-cp", "--csv2parquet", required=False, help="Convert csv to parquet")
+    ap.add_argument("-pc", "--parquet2csv", required=False, help="Convert parquet to csv")
+    ap.add_argument("-cj", "--csv2json", required=False, help="Convert csv to json")
+    ap.add_argument("-s", "--get-schema", required=False, help="Get schema of parquet file")
+    args = vars(ap.parse_args())
     
+    # check convert option
     if args['csv2parquet']:
         # convert csv to parquet
         if is_file_ext_correct('csv2parquet', args['csv2parquet'], 'csv'):
             inputFilename = args['csv2parquet']
-            outputFilename = inputFilename.split('.')[0] + "_converted.parquet"
+            outputFilename = add_filename_suffix(inputFilename, 'converted', 'parquet')
             
             csv_to_parquet(inputFilename, outputFilename)
     elif args['parquet2csv']:
         # convert parquet to csv
         if is_file_ext_correct('parquet2csv', args['parquet2csv'], 'parquet'):
             inputFilename = args['parquet2csv']
-            outputFilename = inputFilename.split('.')[0] + "_converted.csv"
+            outputFilename = add_filename_suffix(inputFilename, 'converted', 'csv')
             
             parquet_to_csv(inputFilename, outputFilename)
     elif args['csv2json']:
         # convert csv to json
         if is_file_ext_correct('csv2json', args['csv2json'], 'csv'):
             inputFilename = args['csv2json']
-            outputFilename = inputFilename.split('.')[0] + "_converted.json"
+            outputFilename = add_filename_suffix(inputFilename, 'converted', 'json')
             
             csv_to_json(inputFilename, outputFilename)
     elif args['get-schema']:
