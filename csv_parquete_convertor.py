ff49fec354f14838a5e6e933b467c0e824b7fb63
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-06-29 12:08:03 +0300

    Add command line arguments

--- csv_parquete_convertor.py
+++ csv_parquete_convertor.py
@@ -1,11 +1,22 @@
+# import the necessary packages
 import pandas as pd
 import csv, json
+import argparse
+from pathlib import Path
+ 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-cp", "--csv2parquet", required=False, help="Convert csv to parquet")
+ap.add_argument("-pc", "--parquet2csv", required=False, help="Convert parquet to csv")
+ap.add_argument("-cj", "--csv2json", required=False, help="Convert csv to json")
+ap.add_argument("-s", "--get-schema", required=False, help="Get schema of parquet file")
+args = vars(ap.parse_args())
 
-def pd_csv_to_parquet(csv_path: str, parquet_path: str):
+print(args)
+
+def csv_to_parquet(csv_path: str, parquet_path: str):
     df = pd.read_csv(csv_path)
     df.to_parquet(parquet_path)
-    
-    return df
 
 def pd_parquet_to_csv(parquet_path: str, csv_path: str):
     df = pd.read_parquet(parquet_path)
@@ -26,9 +37,22 @@ def csv_to_json(csv_path:str, json_path:str, json_indent=4):
         jsonFile.write(json.dumps(data, indent=json_indent))
 
 def main():
-    pd_csv_to_parquet('data.csv', 'data.parquet')
-    pd_parquet_to_csv('data.parquet', 'data_conv.csv')
-    csv_to_json('data.csv','data.json')
+    
+    if args['csv2parquet']:
+        # convert csv to parquet
+        inputFilename = args['csv2parquet']
+        p = Path(inputFilename)
+        outputFilename = p.with_suffix('.parquet')
+        
+        csv_to_parquet(inputFilename, outputFilename)
+        print(f'Successfully converted from {inputFilename} to {outputFilename}')
+        
+    elif args['parquet2csv']:
+        print('b')
+    
+    # pd_csv_to_parquet('data.csv', 'data.parquet')
+    # pd_parquet_to_csv('data.parquet', 'data_conv.csv')
+    # csv_to_json('data.csv','data.json')
 
 if __name__ == "__main__":
     main()
